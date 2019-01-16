a8951045c1a585ba296b4201794729c54740d53d
Eric Ridgeway <ericridgeway@gmail.com>
2019-01-15 22:21:52 -0500

    Add version tool

--- gen_module/mix.exs
+++ gen_module/mix.exs
@@ -23,6 +23,7 @@ defmodule GenModule.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:version_tasks, "~> 0.11.3"},
     ]
   end
 end
