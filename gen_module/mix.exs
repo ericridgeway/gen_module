94419da8d139a009480ab51f805b22c1c4e1e6f1
Eric Ridgeway <ericridgeway@gmail.com>
2019-01-15 23:28:13 -0500

    Update to 0.1.2

--- gen_module/mix.exs
+++ gen_module/mix.exs
@@ -4,7 +4,7 @@ defmodule GenModule.MixProject do
   def project do
     [
       app: :gen_module,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
